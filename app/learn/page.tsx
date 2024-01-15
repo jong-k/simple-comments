7f92a42ff981b18aee768226afa56ffda9c7186f
jong-k <fully.charged07@gmail.com>
2024-01-15 23:46:30 +0900

    feat: learn, passing props 링크 추가

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Learn() {
-  return <div>Hello from Learn page</div>;
+  return (
+    <div>
+      <Link href="/learn/passing-props">
+        <h2 className="text-2xl">props 전달하기</h2>
+      </Link>
+    </div>
+  );
 }
