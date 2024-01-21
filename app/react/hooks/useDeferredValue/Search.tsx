1fa66a1afcb5bc4688afa06b68469771c29822a3
jong-k <fully.charged07@gmail.com>
2024-01-21 20:08:08 +0900

    feat: stale data UI 변경

--- app/react/hooks/useDeferredValue/Search.tsx
+++ app/react/hooks/useDeferredValue/Search.tsx
@@ -4,6 +4,7 @@ import SearchResults from "./SearchResults";
 export default function Search() {
   const [query, setQuery] = useState("");
   const deferredQuery = useDeferredValue(query);
+  const isStale = query !== deferredQuery;
   return (
     <div className="w-full h-full bg-amber-200 p-10">
       <label>
@@ -11,7 +12,16 @@ export default function Search() {
         <input value={query} onChange={(e) => setQuery(e.target.value)} />
       </label>
       <Suspense fallback={<h2>Loading...</h2>}>
-        <SearchResults query={deferredQuery} />
+        <div
+          style={{
+            opacity: isStale ? 0.5 : 1,
+            transition: isStale
+              ? "opacity 0.2s 0.2s linear"
+              : "opacity 0s 0s linear",
+          }}
+        >
+          <SearchResults query={deferredQuery} />
+        </div>
       </Suspense>
     </div>
   );
