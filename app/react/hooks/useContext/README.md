fceeee2fa1b8e7ba0723f1219dc3060f0c77889d
jong-k <fully.charged07@gmail.com>
2024-01-22 23:35:02 +0900

    feat: user context 추가

--- app/react/hooks/useContext/README.md
+++ app/react/hooks/useContext/README.md
@@ -8,3 +8,5 @@
 과제
 - [x] 테마 context(다크/라이트) 읽고 구독하기
   - ThemeContext 만들기
+- [x] user state와 setState 함수를 객체 리터럴로 context에 담기
+  - UserContext 만들기
--- app/react/hooks/useContext/SimpleForm.tsx
+++ app/react/hooks/useContext/SimpleForm.tsx
@@ -1,30 +1,44 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, SetStateAction, useContext, useState } from "react";
 
 const ThemeContext = createContext<Theme | null>(null);
+const CurrentUserContext = createContext<UserContext | null>(null);
 
 type Theme = "dark" | "light";
+interface User {
+  name: string;
+}
+interface UserContext {
+  currentUser: User | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User>>;
+}
 
 export default function SimpleForm() {
   const [theme, setTheme] = useState<Theme>("light");
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
   return (
     <ThemeContext.Provider value={theme}>
-      <WelcomePanel />
-      <label>
-        <input
-          type="checkbox"
-          checked={theme === "dark"}
-          onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-        />
-        Use dark mode
-      </label>
+      <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+        <WelcomePanel />
+        <label>
+          <input
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+          />
+          Use dark mode
+        </label>
+      </CurrentUserContext.Provider>
     </ThemeContext.Provider>
   );
 }
 
 function WelcomePanel() {
+  const currentUserContext = useContext(CurrentUserContext);
+  const { currentUser } = currentUserContext!;
   return (
     <Panel title="Welcome!">
-      <div>ho</div>
+      {currentUser !== null ? <Greeting /> : <LoginForm />}
     </Panel>
   );
 }
@@ -43,5 +57,67 @@ function Panel({
       <h1>{title}</h1>
       {children}
     </section>
+  );
+}
+
+function Greeting() {
+  const currentUserContext = useContext(CurrentUserContext);
+  const { currentUser } = currentUserContext!;
+  return <p>You logged in as {currentUser?.name}.</p>;
+}
+
+function LoginForm() {
+  const currentUserContext = useContext(CurrentUserContext);
+  const { setCurrentUser } = currentUserContext!;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const canLogin = firstName !== "" && lastName !== "";
+  return (
+    <>
+      <label>
+        First name{": "}
+        <input
+          className="bg-amber-200"
+          required
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </label>
+      <label>
+        Last name{": "}
+        <input
+          className="bg-amber-200"
+          required
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <Button
+        disabled={!canLogin}
+        onClick={() => {
+          setCurrentUser({
+            name: firstName + " " + lastName,
+          });
+        }}
+      >
+        Log in
+      </Button>
+      {!canLogin && <i>Fill in both fields.</i>}
+    </>
+  );
+}
+
+interface ButtonProps {
+  children: React.ReactNode;
+  disabled: boolean;
+  onClick: () => void;
+}
+
+function Button({ children, disabled, onClick }: ButtonProps) {
+  const theme = useContext(ThemeContext);
+  return (
+    <button className={`panel-${theme}`} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
   );
 }
