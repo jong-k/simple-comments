f77725079461b7c0c15905e7249f34b82eee62f4
jong-k <fully.charged07@gmail.com>
2024-01-29 18:11:42 +0900

    docs: 상호작용 파트 추가

---
+++ app/learn/interactivity/README.md
@@ -0,0 +1,8 @@
+# Adding Interactivity
+- Responding to events
+- State: a component’s memory
+- Render and commit
+- State as a snapshot
+- Queueing a series of state updates
+- Updating objects in state
+- Updating arrays in state
---
+++ app/learn/interactivity/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Interactivity() {
+  return (
+    <div>
+      <Link href="/learn/interactivity/responding-events">
+        <h2>이벤트에 응답하기</h2>
+      </Link>
+    </div>
+  );
+}
