f42ebd79cf06d03cb26b9a990d758963db279813
jong-k <fully.charged07@gmail.com>
2024-02-22 14:28:44 +0900

    feat: effect lifecycle 페이지 추가

---
+++ app/learn/escape-hatches/effects-lifecycle/page.tsx
@@ -0,0 +1,3 @@
+export default function EffectsLifecycle() {
+  return <div></div>;
+}
--- app/learn/escape-hatches/page.tsx
+++ app/learn/escape-hatches/page.tsx
@@ -6,6 +6,9 @@ export default function EscapeHatches() {
       <Link href="/learn/escape-hatches/referencing-refs">
         <h2>Ref로 값 참조하기</h2>
       </Link>
+      <Link href="/learn/escape-hatches/effects-lifecycle">
+        <h2>Effect 라이프사이클</h2>
+      </Link>
     </div>
   );
 }
